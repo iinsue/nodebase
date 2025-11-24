a9336a451aff2511d762312ecdfaacc74bf15fdf
insu <hinsu93@gmail.com>
2025-11-24 21:24:57 +0900

    #5-2 add Login & Signup

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/features/auth/components/login-form";
+
+const Page = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-  await queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+const Page = () => {
+  const { data } = authClient.useSession();
 
   return (
-    <div className="flex min-h-screen min-w-screen items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
-    </div>
+    <>
+      <div className="flex min-h-screen min-w-screen items-center justify-center">
+        {JSON.stringify(data)}
+
+        {data && <Button onClick={() => authClient.signOut()}>Logout</Button>}
+      </div>
+    </>
   );
 };
 
