2576df04b1d43db4036a73f409a80ba2c471f2dd
insu <hinsu93@gmail.com>
2025-11-24 22:24:16 +0900

    #5-4 Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import { caller } from "@/trpc/server";
 import { requireAuth } from "@/lib/auth-utils";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
     <>
-      <div className="flex min-h-screen min-w-screen items-center justify-center">
+      <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-y-6">
         protected server component
+        <div>{JSON.stringify(data, null, 2)}</div>
+        <LogoutButton />
       </div>
     </>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,14 @@
 import prisma from "@/lib/db";
 
-import { baseProcedure, createTRPCRouter } from "../init";
+import { baseProcedure, createTRPCRouter, protectProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query((opts) => {
-    return prisma.user.findMany();
+  getUsers: protectProcedure.query(({ ctx }) => {
+    return prisma.user.findMany({
+      where: {
+        id: ctx.auth.user.id,
+      },
+    });
   }),
 });
 // export type definition of API
