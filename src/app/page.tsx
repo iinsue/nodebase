259ecb22c1313cf95a5a92313a82980db5f40bc4
insu <hinsu93@gmail.com>
2026-04-19 09:55:28 +0900

    04: Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import { caller } from "@/trpc/server";
 import { requireAuth } from "@/lib/auth-utils";
+
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
     <>
-      <div className="flex min-h-screen min-w-screen items-center justify-center">
-        protected server component
+      <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-y-6">
+        <div>protected server component</div>
+        <div>{JSON.stringify(data, null, 2)}</div>
+        <LogoutButton />
       </div>
     </>
   );
