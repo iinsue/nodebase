653ef4245b780b33e8d1eb5d6e73f311aec983ac
insu <hinsu93@gmail.com>
2026-04-20 23:24:59 +0900

    06: add workflow model & test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
-import { caller } from "@/trpc/server";
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
 
+import { Button } from "@/components/ui/button";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
 
   return (
     <>
       <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-y-6">
         <div>protected server component</div>
         <div>{JSON.stringify(data, null, 2)}</div>
+
+        <Button disabled={create.isPending} onClick={() => create.mutate()}>
+          Create workflow
+        </Button>
+
         <LogoutButton />
       </div>
     </>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -10,6 +10,18 @@ export const appRouter = createTRPCRouter({
       },
     });
   }),
+
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany();
+  }),
+
+  createWorkflow: protectedProcedure.mutation(() => {
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow",
+      },
+    });
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
