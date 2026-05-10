8a693d745c84a86a5d9b386c6c602bc86a7de518
insu <hinsu93@gmail.com>
2026-05-10 18:36:32 +0900

    17: Execution node 삭제기능추가

--- src/features/executions/components/base-execution-node.tsx
+++ src/features/executions/components/base-execution-node.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { memo, type ReactNode } from "react";
 import type { LucideIcon } from "lucide-react";
-import { type NodeProps, Position } from "@xyflow/react";
+import { type NodeProps, Position, useReactFlow } from "@xyflow/react";
 
 import { WorkflowNode } from "@/components/workflow-node";
 import { BaseHandle } from "@/components/react-flow/base-handle";
@@ -29,8 +29,26 @@ export const BaseExecutionNode = memo(
     onSettings,
     onDoubleClick,
   }: BaseExecutionNodeProps) => {
-    // TODO: add delete method
-    const handleDelete = () => {};
+    const { setNodes, setEdges } = useReactFlow();
+
+    // 삭제기능
+    const handleDelete = () => {
+      // Node 업데이트
+      setNodes((currentNodes) => {
+        const updatedNodes = currentNodes.filter((node) => node.id !== id);
+
+        return updatedNodes;
+      });
+
+      // Edge 업데이트
+      setEdges((currentEdges) => {
+        const updatedEdges = currentEdges.filter(
+          (edge) => edge.source !== id && edge.target !== id,
+        );
+
+        return updatedEdges;
+      });
+    };
 
     return (
       <WorkflowNode
