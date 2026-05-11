7a3743bce4b3114119d2fa1f850355f5d9dc4c9e
insu <hinsu93@gmail.com>
2026-05-11 22:37:31 +0900

    18: node execution

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,14 +1,52 @@
+import ky, { type Options as KyOptions } from "ky";
+import { NonRetriableError } from "inngest";
+
 import type { NodeExecutor } from "@/features/executions/types";
 
-type HttpRequestData = Record<string, unknown>;
+type HttpRequestData = {
+  endpoint?: string;
+  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  body?: string;
+};
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
+  data,
   nodeId,
   context,
   step,
 }) => {
   // TODO: Publish "loading" state for http request
-  const result = await step.run("http-request", async () => context);
+
+  if (!data.endpoint) {
+    // TODO: Publish "error" state for http request
+    throw new NonRetriableError("HTTP Request node: No endpoint configured");
+  }
+
+  const result = await step.run("http-request", async () => {
+    const endpoint = data.endpoint!;
+    const method = data.method || "GET";
+
+    const options: KyOptions = { method };
+
+    if (["POST", "PUT", "PATCH"].includes(method)) {
+      options.body = data.body;
+    }
+
+    const response = await ky(endpoint, options);
+    const contentType = response.headers.get("content-type");
+    const responseData = contentType?.includes("application/json")
+      ? await response.json()
+      : await response.text();
+
+    return {
+      ...context,
+      httpResponse: {
+        status: response.status,
+        statusText: response.statusText,
+        data: responseData,
+      },
+    };
+  });
   // TODO: Pulbish "success" stat for http request
   return result;
 };
