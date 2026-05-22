6733b3154fc9bcc7a4562ee0a6ad052f4c45f3a0
insu <hinsu93@gmail.com>
2026-05-22 22:33:42 +0900

    25: Add Page - Server loader

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,9 +1,32 @@
+import { Suspense } from "react";
+import { SearchParams } from "nuqs";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { HydrateClient } from "@/trpc/server";
 import { requireAuth } from "@/lib/auth-utils";
 
-const Page = async () => {
+import { prefetchCredentials } from "@/features/credentials/server/prefetch";
+import { credentialsParamsLoader } from "@/features/credentials/server/params-loader";
+
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
 
-  return <p>Credentials</p>;
+  const params = await credentialsParamsLoader(searchParams);
+  prefetchCredentials(params);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <p>TODO: Credentials list</p>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
