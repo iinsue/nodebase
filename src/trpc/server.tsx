030275608ef56e3ef1199e832fd2aa53e07b95f5
insu <hinsu93@gmail.com>
2026-05-28 19:02:27 +0900

    30: deployment

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -23,14 +23,6 @@ export const trpc = createTRPCOptionsProxy({
   queryClient: getQueryClient,
 });
 
-// If your router is on a separate server, pass a client:
-createTRPCOptionsProxy<AppRouter>({
-  client: createTRPCClient<AppRouter>({
-    links: [httpLink({ url: "..." })],
-  }),
-  queryClient: getQueryClient,
-});
-
 export const caller = appRouter.createCaller(createTRPCContext);
 
 // Prefetch
